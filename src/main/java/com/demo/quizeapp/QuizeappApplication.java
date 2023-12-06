f6d91863f32d81746b92d905a024c6a9daa9894a
rusha-99 <tharushapathirana07@gmail.com>
2023-12-06 14:47:46 +0530

    Initialized the project

---
+++ src/main/java/com/demo/quizeapp/QuizeappApplication.java
@@ -0,0 +1,13 @@
+package com.demo.quizeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizeappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/quizeapp/QuizeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.quizeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
